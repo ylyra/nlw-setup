540d6d4c3142d7897b8ccad9d06d3a408659f23c
Yan Lyra <yan.lyra@growth.sale>
2023-01-22 00:23:20 -0300

    feat: created habit day on mobile and the progress icon

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,43 @@
-import { View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateDaysBefore } from "../utils/generate-dates-from-year-beginning";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const summaryDates = generateDaysBefore();
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, index) => (
+          <Text
+            key={`${weekDay}-${index}`}
+            className="text-white text-xl text-center mx-1"
+            style={{
+              width: DAY_SIZE,
+            }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 48,
+        }}
+      >
+        <View className="flex-row flex-wrap">
+          {summaryDates.map((date) => (
+            <HabitDay key={date.toISOString()} date={date} />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
