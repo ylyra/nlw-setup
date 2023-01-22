8ea8f221614ae4a8650251bd820c5c85da9fef72
Yan Lyra <yan.lyra@growth.sale>
2023-01-22 00:54:06 -0300

    feat: added summary route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -125,4 +125,35 @@ export async function appRoutes(app: FastifyInstance) {
       return createResponseError(error, ctx);
     }
   });
+
+  app.get("/summary", async (request, ctx) => {
+    try {
+      const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+      return summary;
+    } catch (error) {
+      return createResponseError(error, ctx);
+    }
+  });
 }
