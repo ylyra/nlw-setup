f0fd9d690780b856d961986a1ad24d30aec72332
Yan Lyra <yan.lyra@growth.sale>
2023-01-22 04:28:06 -0300

    feat: created static frontend for habit page on mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { ScrollView, Text, View } from "react-native";
 
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
@@ -8,6 +9,8 @@ const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const summaryDates = generateDaysBefore();
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -34,7 +37,15 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {summaryDates.map((date) => (
-            <HabitDay key={date.toISOString()} date={date} />
+            <HabitDay
+              key={date.toISOString()}
+              date={date}
+              onPress={() =>
+                navigate("habit", {
+                  date: date.toISOString(),
+                })
+              }
+            />
           ))}
         </View>
       </ScrollView>
